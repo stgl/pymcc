d47ae7c760ff2fc4172ff8299fe2f42971400702
George Hilley <gehilley@gmail.com>
2019-05-13 15:37:24 -0700

    reorg

--- libmcc-lidar/cmake_install.cmake
+++ libmcc-lidar/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/hilley/Documents/GitHub/pymcc/mcciter
+# Install script for directory: /Users/hilley/Documents/GitHub/pymcc/libmcc
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/hilley/Documents/GitHub/pymcc/mcciter/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/hilley/Documents/GitHub/pymcc/libmcc/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
