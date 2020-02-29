9a525a33e343d16cc241ba413024dbaac6e2c8c5
rmsare <robertmsare@gmail.com>
2020-02-29 07:55:32 -0800

    Clean up GS and move to solvers.h

--- libmcc_lidar/tpsdemo/solvers.h
+++ libmcc_lidar/tpsdemo/solvers.h
@@ -1,5 +1,5 @@
-#ifndef GAUSSSEIDEL_H
-#define GAUSSSEIDEL_H
+#ifndef SOLVERS_H
+#define SOLVERS_H
 
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/matrix_proxy.hpp>
@@ -8,10 +8,11 @@
 
 // Solve a linear equation system a*x=b using the Gauss Seidel method 
 //
-// Possible return values:
-//  0=success
-//  1=singular matrix
-//  2=a.rows != b.rows
+// Returns the solution vector x
+//
+// Throws an exception if
+// 1. a and b do not have the same number of rows
+// 2. a has zero entires on its main diagonal
 //
 template <typename T> int Gauss_Seidel_Solve(
   boost::numeric::ublas::matrix<T>& a,
@@ -19,17 +20,22 @@ template <typename T> int Gauss_Seidel_Solve(
   int n_iter)
 {
   
-  int nrows = a.size1(), ncols = a.size2();
+  int nrows = a.size1();
   boost::numeric::ublas::zero_matrix<T> x(nrows, 1);
   boost::numeric::ublas::zero_matrix<T> n(nrows, 1);
 
   if (a.size1() != b.size1())
-    throw SingularMatrixError();  
+    throw SingularMatrixError();
+
+  // Check singluarity
+  for (int j = 0; j < n; ++j)
+    if (a(j,j) == 0)
+      throw SingularMatrixError();
 
   while (n_iter > 0) {
-    for (i = 0; i < p; i++) {
+    for (i = 0; i < nrows; i++) {
       n[i] = (b[i] / a(i,i));
-      for (j = 0; j < p; j++) {
+      for (j = 0; j < nrows; j++) {
         if (j == i)
           continue;
         n[i] = n[i] - ((a(i,j) / a(i,i)) * x[j]);
@@ -39,13 +45,9 @@ template <typename T> int Gauss_Seidel_Solve(
     n_iter--;
   }
 
-  // Check singluarity
-  for (int j = 0; j < n; ++j)
-    if (a(j,j) == 0)
-      throw SingularMatrixError();  
-
   return x;
 }
 
-#endif // GAUSSSEIDEL_H
+
+#endif // SOLVERS_H
 
